305a6cebfe3e1f21a709a0aef946aa92faf2ce8d
Sandor Teglas <sandor@artkonekt.com>
2016-04-28 10:46:50 +0300

    Resque workers are created without loggers becasue Kamisama's php-resque-ex has a different logging mechanism, to be adjusted. Instead, the ContainerAwareJob has now a log() method that logs to the standard output that gets written to log file found in the /app/logs folder.

--- ContainerAwareJob.php
+++ ContainerAwareJob.php
@@ -5,6 +5,7 @@ namespace Konekt\ResqueExBundle;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 abstract class ContainerAwareJob extends Job
 {
@@ -56,4 +57,23 @@ abstract class ContainerAwareJob extends Job
             $this->kernel->shutdown();
         }
     }
+
+    /**
+     * Logs a message to the standard output.
+     *
+     * @param   string   $message
+     *
+     * @param   bool     $useTimestamp
+     */
+    public function log($message, $useTimestamp = true)
+    {
+        $output = '';
+        if ($useTimestamp) {
+            $output = '[' . (new \DateTime())->format('Y:m:d H:i:s') . ']: ';
+        }
+        $output .= $message;
+
+        print '[job] ' . $output . "\n";
+    }
+
 }
